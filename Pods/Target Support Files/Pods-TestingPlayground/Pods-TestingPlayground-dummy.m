f632f3e94c9814c2f01a7098236290bf1ca97c1e
nguyenductho <nguyenductho89@gmail.com>
2020-08-29 08:24:54 +0700

    #Playground to app

--- Pods/Target Support Files/Pods-TestingPlayground/Pods-TestingPlayground-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestingPlayground : NSObject
-@end
-@implementation PodsDummy_Pods_TestingPlayground
-@end
---
+++ SideApps/Pods/Target Support Files/Pods-SideApps/Pods-SideApps-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SideApps : NSObject
+@end
+@implementation PodsDummy_Pods_SideApps
+@end
